890b12a1737489251c6071983ec130309d6eb753
euclidesseg <euder0206@gmail.com>
2023-05-02 22:55:57 -0500

    se crea un obcervable para que emita el valor de darkModeIsSelect hacia loos demas componentes

--- src/app/briefcase/pages/about-me/about-me.component.ts
+++ src/app/briefcase/pages/about-me/about-me.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.css']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit{
+  darckModeSelect:boolean = false
+  constructor(private sharedService:SharedService){}
+  
+  ngOnInit(): void {
+    this.sharedService.darkModeIsSelect
+    .subscribe((value) => {this.darckModeSelect = value})
+    
+  }
+
 
 }
--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -1,12 +1,16 @@
 import { DOCUMENT } from '@angular/common';
 import { Injectable, OnInit, Inject} from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService implements OnInit {
   close: boolean = false;
-  darkModeIsSelect: boolean = false
+
+  private _darkModeIsSelect = new BehaviorSubject<boolean>(false);
+  darkModeIsSelect :Observable<boolean> =  this._darkModeIsSelect.asObservable()
+
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
@@ -20,14 +24,26 @@ export class SharedService implements OnInit {
     return this.close
   }
 
-  selecDarkMode():{isActive: boolean, modeText:string}{
-    this.darkModeIsSelect = !this.darkModeIsSelect
-    if (this.darkModeIsSelect == true) {
-        this.document.body.classList.add('vela__blue');
-        return {isActive:true, modeText:'Ligth Mode'}
-      } else {
-        this.document.body.classList.remove('vela__blue');
-        return {isActive:false, modeText:'Dark Mode'}
+   selecDarkMode(): { isActive: boolean, modeText: string } {
+    const isActive = !this._darkModeIsSelect.value;
+    if (isActive) {
+      this.document.body.classList.add('vela__blue');
+      this._darkModeIsSelect.next(true);
+      return { isActive: true, modeText: 'Light Mode' };
+    } else {
+      this.document.body.classList.remove('vela__blue');
+      this._darkModeIsSelect.next(false);
+      return { isActive: false, modeText: 'Dark Mode' };
     }
   }
 }
+
+// NOTAS: ( darkModeIsSelect) es una variable publica que emite un  observable, el cual recibe mediante asObservable(),
+// el ultimo valor emitido por (_darkModeIsSelect).
+// selecDarkMode() es el método que cambia el valor del objeto observable (_darkModeIsSelect) y emite el nuevo valor 
+
+// El método next() de BehaviorSubject es utilizado para actualizar el valor almacenado 
+// en el objeto observable y notificar a sus suscriptores del cambio.
+
+// . El método asObservable() de la clase BehaviorSubject crea y devuelve un observable que emite el último valor emitido por el objeto
+// en este caso el objeto es  _darkModeIsSelect
