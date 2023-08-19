b1a934c45312cf51827605768e21a925317c0144
euclidesseg <euder0206@gmail.com>
2023-08-19 14:22:34 -0500

    Se completa la seccion de estudios

---
+++ src/app/briefcase/data/studies.data.ts
@@ -0,0 +1,43 @@
+export const studies = [
+    {
+      img: 'https://mir-s3-cdn-cf.behance.net/projects/404/deac98138334863.Y3JvcCwxMzgwLDEwODAsMjcwLDA.png',
+      date: 'June - 2021 | June - 2023',
+      institution: 'cesde',
+      title: 'Software Development Technician',
+      aptitudes: [
+        'HTML, CSS, JavaScript, Java, C#, GitHub, Python, Logica de Programacion, Analisis de Datos, SQLServer, MySql React, Angular',
+      ],
+      credential: '',
+    },
+    {
+      img: 'https://cdn6.aptoide.com/imgs/1/6/9/169eb96640029c9984161f09c34f7109_icon.png',
+      date: 'July - 2023 | August - 2023',
+      institution: 'Coursera',
+      title: 'Introducctión to Git and GitHub',
+      aptitudes: [
+        'Git, GitHub, Python, QuickLabs',
+      ],
+      credential: 'https://www.coursera.org/account/accomplishments/certificate/DDQX2D48MG27',
+    },
+
+    {
+      img: 'https://cdn6.aptoide.com/imgs/1/6/9/169eb96640029c9984161f09c34f7109_icon.png',
+      date: 'April - 2023 | June - 2023',
+      institution: 'coursera',
+      title: 'Crash  Course on Python',
+      aptitudes: [
+        'Spyder, Python Algorithms, OPP'
+      ],
+      credential: 'https://www.coursera.org/account/accomplishments/certificate/VWDZ9B9DUJD3',
+    },
+    {
+      img: 'https://cdn6.aptoide.com/imgs/1/6/9/169eb96640029c9984161f09c34f7109_icon.png',
+      date: 'April - 2023 | June - 2023',
+      institution: 'coursera',
+      title: 'Introducción al HTML5',
+      aptitudes: [
+        'HTML5, Visual Studio Code, GitHub, CSS3'
+      ],
+      credential: 'https://www.coursera.org/account/accomplishments/certificate/AXSNK5ZVSQZQ',
+    },
+  ];
--- src/app/briefcase/pages/studies-page/studies-page.component.ts
+++ src/app/briefcase/pages/studies-page/studies-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import  {studies } from '../../data/studies.data'
+import { DarkModeService } from 'src/app/shared/services/dark-mode.service';
 
 @Component({
   selector: 'app-studies-page',
   templateUrl: './studies-page.component.html',
-  styleUrls: ['./studies-page.component.css']
+  styleUrls: ['./studies-page.component.css'],
 })
 export class StudiesPageComponent {
-
+  public darkModeSelected: boolean = false;
+  public studies = studies;
+  constructor(private darkService : DarkModeService ){
+    this.darkService.darkModeIsSelect.subscribe((value) =>  {
+      if(value.mode === 'dark')(
+        this.darkModeSelected = true
+      )
+      else{
+        this.darkModeSelected = false;
+      }
+    })
+  }
 }
