f3b3402326aafd3a3e26284c9dbd8ed0216a5060
euclidesseg <euder0206@gmail.com>
2024-03-17 21:49:13 -0500

    update experiences date

--- src/app/briefcase/data/experiences.data.ts
+++ src/app/briefcase/data/experiences.data.ts
@@ -3,7 +3,7 @@ import { Experiences } from "src/app/models/experiences.model";
 export const experiences:Experiences[] = [
   {
     img:'https://media.licdn.com/dms/image/C560BAQFStGE2TteAOQ/company-logo_200_200/0/1536875607808?e=2147483647&v=beta&t=ufyE6JNtYZvEUfVJCOIt0iOhFufnJAY4uLyb4fSvgTQ',
-    date:'20-09-2022 | Actual',
+    date:'20-09-2023 | Actual',
     company:'101 Software  s.a.s',
     title:'Developer JR',
     skills:['Direct collaboration with development team.',
@@ -13,13 +13,23 @@ export const experiences:Experiences[] = [
   },
   {
     img:'https://firebasestorage.googleapis.com/v0/b/portfolio-898c3.appspot.com/o/Intecol.jpg?alt=media&token=e253f345-bf7f-4af6-8144-38e120e4c46e',
-    date:'16-12-2022 | 19-09-2022',
+    date:'16-06-2023 | 19-09-2023',
     company:'Iintecol  s.a.s',
     title:'Developer JR',
     skills:['Direct collaboration with the development team to analyze customer requirements.',
       'Effective coding of software changes and alterations based on specific design specifications.',
       'Handling web requests, RxJs operators for data transformation.',
       'Generation of report graphs for budget and plant production during the current project.'],
+  },
+  {
+    img:'https://firebasestorage.googleapis.com/v0/b/portfolio-898c3.appspot.com/o/Intecol.jpg?alt=media&token=e253f345-bf7f-4af6-8144-38e120e4c46e',
+    date:'16-12-2022 | 15-06-2023',
+    company:'   s.a.s',
+    title:'Developer Traine',
+    skills:['Code corrections and implementation of front-end functionalities applying technologies such as Angular and Prime NG.',
+      'Versioning of the source code through branches in asure devops repositories.',
+      'Agile methodologies (Scrum).',
+      ],
   },
   {
     img:'https://firebasestorage.googleapis.com/v0/b/portfolio-898c3.appspot.com/o/Mnaranja.png?alt=media&token=344b460b-f783-4495-af4c-f12ffae3d648',
